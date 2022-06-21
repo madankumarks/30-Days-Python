8ee0006a30b25cd3b38ccba8f4c6ff2da11ef494
Madankumar <99166463+madankumarks@users.noreply.github.com>
2022-06-21 20:11:05 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,20 @@
+# 100days-Code-Python
+#Cheat sheet
+
+#Day-1
+input_string=input()
+print('Hello, World.')
+print(input_string)
+
+#Day-2
+i = 4
+d = 4.0
+s = 'HackerRank '
+a=int(input())
+b=float(input())
+c=str(input())
+print(i+a)
+print(d+b)
+print(s+c)
+
+#Day-3
