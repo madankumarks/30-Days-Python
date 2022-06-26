8e80f5b25de29afd9e20027362ab763ce6cca82f
Madankumar <99166463+madankumarks@users.noreply.github.com>
2022-06-26 18:24:44 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -115,7 +115,18 @@ if __name__ == '__main__':
     
     DAy-8
     
-    
+    n = int(input())
+d = {}
+for i in range(n):
+    x = input().split()
+    d[x[0]] = x[1]
+while True:
+    try:
+        name = input()
+        if name in d:
+            print(name, "=", d[name], sep="")
+        else : print("Not found")
+    except: break
     
     
 
